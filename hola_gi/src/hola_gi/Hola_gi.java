3c065189828c2089fb48d2ed30f73cdb6a38d177
usuario <usuario@usuario-HP-Compaq-6000-Pro-MT-PC>
2016-11-21 19:50:52 -0500

    Mi primer importe a git hub

---
+++ hola_gi/src/hola_gi/Hola_gi.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_gi;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author usuario
+ */
+public class Hola_gi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         String nombre="";
+    
+    
+    nombre=JOptionPane.showInputDialog("dime tu nombre");
+    
+    JOptionPane.showMessageDialog(null,"HOLA"+nombre+"como vas");
+    
+        // TODO code application logic here
+    }
+    
+   
+            
+   
+    
+    
+    
+    
+    
+    
+}
